09d49681871d3408e227fee508b855d9c93eb2f0
The Lord Jackson <101205562+thelordman@users.noreply.github.com>
2022-03-21 15:52:09 +0000

    1.0
    Made the base for the bot, as well as 1 command.
    
    Usage:
    !ass: sends a picture of a huge ass.

---
+++ main.py
@@ -0,0 +1,18 @@
+import discord
+
+client = discord.Client()
+token = os.environ['TOKEN']
+
+@client.event
+async def on_ready():
+  print("JDA Ready")
+
+@client.event
+async def on_message(msg):
+  if msg.author == client.user:
+    return
+
+  if msg.content.startswith("!ass"):
+    await msg.channel.send("http://cdn.megabuttpics.com/images/biggest-ssbbw-large-bottom.jpg")
+
+client.run(token)
