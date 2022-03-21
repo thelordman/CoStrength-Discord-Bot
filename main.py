bdded2083651442e59e5a694a79f1c4aef3d7cc2
The Lord Jackson <101205562+thelordman@users.noreply.github.com>
2022-03-21 16:39:24 +0000

    1.1
    The bot responds to many new messages.

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import discord
 import os
+import random
 
 client = discord.Client()
 token = os.environ['TOKEN']
+
+sad_words = ["sad", "depress", "unhappy", "angry", "miserabl", "mad", "kill you", "bad"]
+sad_responses = ["L", "ur so bad", "lmao", "lol", "kys", "damn u suck", "Loser!", "everyone hates you!"]
 
 @client.event
 async def on_ready():
@@ -15,5 +19,11 @@ async def on_message(msg):
 
   if msg.content.startswith("!ass"):
     await msg.reply("http://cdn.megabuttpics.com/images/biggest-ssbbw-large-bottom.jpg")
+
+  if any(word in msg.content.lower() for word in sad_words):
+    await msg.channel.send(random.choice(sad_responses))
+
+  if msg.content.startswith("costrength"):
+    await msg.channel.send("yo")
 
 client.run(token)
